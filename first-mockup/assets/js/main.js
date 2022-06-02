a7d163740f69df74ce7d697452539a995c1d5b28
assasiv <siv0910@gmail.com>
2022-06-03 00:19:17 +0300

    slider added

--- first-mockup/assets/js/main.js
+++ first-mockup/assets/js/main.js
@@ -16,7 +16,8 @@ $('.contact-close').click(function() {
 //     $(".contact-popup").fadeOut('slow');
 // });
 
-$('#submit').click(function(){
+$('#submit').click(function() {
+  console.log("clicked");
     // find('input[type="submit"]')
     $('#submit').val('');
     $('.btn-progress').addClass('active');
@@ -24,6 +25,65 @@ $('#submit').click(function(){
     if($('#submit').val() == 'Sent') {
         $('.btn-progress').removeClass('active');}
  });
+
+
+ $('#slider').slick({
+   dots: true,
+   arrows: false,
+ });
+
+ $('#slider-main').slick({
+  dots: true,
+  arrows: false,
+});
+
+$('#benf-1').click(function() {
+  console.log("text");
+  $("h3.abt-title").removeClass("active"); 
+  $('#benf-1').addClass('active');
+  $('#about-title').text('We turn the idea of legal services');
+  $( "#about-txt" ).text("Consolidating many years of experience and practice, the company's lawyers accompany complex projects and achieve a number of important decisions for the industry. Many of them form the practice of applying the law of the sea in Ukraine.");
+
+ });
+
+$('#benf-2').click(function() {
+  console.log("text");
+  $("h3.abt-title").removeClass("active"); 
+  $('#benf-2').addClass('active');
+  $('#about-title').text('Lorem ipsum dolor sit amet consectetur adipisic');
+  $( "#about-txt" ).text("Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus minus illum ad molestiae cumque debitis numquam laboriosam! Soluta fugiat ex sequi praesentium officiis beatae sint nemo, omnis aliquam, consequuntur maxime.");
+
+ });
+
+ $('#benf-3').click(function() {
+  console.log("text");
+  $("h3.abt-title").removeClass("active"); 
+  $('#benf-3').addClass('active');
+  $('#about-title').text('Necessitatibus minus illum ad molestiae cumque debitis numquam');
+  $( "#about-txt" ).text("Necessitatibus minus illum ad molestiae cumque debitis numquam laboriosam! Soluta fugiat ex sequi praesentium officiis beatae sint nemo, omnis aliquam, consequuntur maxime.Lorem ipsum dolor sit amet consectetur adipisicing elit. ");
+
+ });
+
+ $('#benf-2').click(function() {
+  console.log("text");
+  $("h3.abt-title").removeClass("active"); 
+  $('#benf-2').addClass('active');
+  $('#about-title').text('Lorem ipsum dolor sit amet consectetur adipisic');
+  $( "#about-txt" ).text("Soluta fugiat ex sequi praesentium officiis beatae sint nemo, omnis aliquam, consequuntur maxime.Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus minus illum ad molestiae cumque debitis numquam laboriosam!");
+
+ });
+
+ $('#benf-4').click(function() {
+  console.log("text");
+  $("h3.abt-title").removeClass("active"); 
+  $('#benf-4').addClass('active');
+  $('#about-title').text('Soluta fugiat ex sequi praesentium officiis beatae sint nemo, omnis aliquam, consequuntur maxime');
+  $( "#about-txt" ).text("Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus minus illum ad molestiae cumque debitis numquam laboriosam! Soluta fugiat ex sequi praesentium officiis beatae sint nemo, omnis aliquam, consequuntur maxime.");
+
+ });
+
+
+
 
 
 
