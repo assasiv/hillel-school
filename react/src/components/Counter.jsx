9d261c41e8accc321298b29688fa1b8caae72c42
assasiv <siv0910@gmail.com>
2023-11-19 22:39:37 +0200

    homework 3 - create counter component

--- react/src/components/Counter.jsx
+++ react/src/components/Counter.jsx
@@ -5,11 +5,14 @@ import './Counter.css';
 const Counter = () => {
     const [counter, setCounter] = useState(0);
     const increment = () => setCounter (counter + 1);
-    const decrement = () => setCounter (counter - 1);
+    // const decrement = () => setCounter (counter - 1);
+    const decrement = () => {
+        setCounter(Math.max(0, counter - 1));
+    }
 
     return (
         <div className="counter">
-            <h2>Hello, your total ckicks is <span className="focus">{counter}</span> times</h2>
+            <h2>Hello, your total clicks are <span className="focus">{counter}</span></h2>
             <div className="btns">
                 <button className="btn-inc" onClick={increment}>Click on me to increase clicks</button>
                 <button className="btn-dec" onClick={decrement}>Click on me to decrease clicks</button>
